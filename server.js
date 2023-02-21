5ea72d0f98a4ad52759a727ef01c031eb0a0768a
chloegithub <miss_jihyun@hotmail.com>
2023-02-21 16:56:50 -0500

    server and index updated

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+let notes = require('./db/db.json')
+
+const PORT = process.env.port || 3001;
+
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for feedback page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get("/api/notes", (req, res) => {
+    res.json(notes) 
+})
+
+app.post("/api/notes", (req, res) => {
+    req.body.id=Math.floor(Math.random()*1000000)
+    notes.push (req.body)
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes))
+    res.json(notes) 
+})
+
+app.delete("/api/notes/:id", (req, res) => {
+  for(i=0; i<notes.length;i++){
+    if(req.params.id==notes[i].id){
+      notes.splice(i,1)
+    }
+  }
+  fs.writeFileSync("./db/db.json", JSON.stringify(notes))
+  res.json(notes) 
+}) 
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
